8b3c3cdbc4a004e5a2480923f8f307c508bb5e0f
wangkun <kun.wang@yoho.cn>
2018-09-19 16:01:06 +0800

    update

--- src/main/java/com/imooc/service/GirlService.java
+++ src/main/java/com/imooc/service/GirlService.java
@@ -1,5 +1,6 @@
 package com.imooc.service;
 
+import com.imooc.common.response.PageResponse;
 import com.imooc.domain.Girl;
 import com.imooc.enums.ResultEnum;
 import com.imooc.exception.GirlException;
@@ -8,6 +9,8 @@ import com.imooc.reposiitory.GirlRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * Created by wangkun on 2016/12/15.
@@ -44,5 +47,14 @@ public class GirlService {
         } else if (age > 10 && age < 16) {
             throw new GirlException(ResultEnum.MIDDLE_SCHOOL);
         }
+    }
+
+    public PageResponse<Girl> getGrilPageList(Girl girl) {
+        int count = girlMapper.selectCount(girl);
+        if (count == 0) {
+            return new PageResponse<>();
+        }
+        List<Girl> girls = girlMapper.getGirlPageList(girl, girl.toPageModel());
+        return new PageResponse<>(count, girls);
     }
 }
